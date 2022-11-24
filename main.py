cfbd73c41fdc7b81cfae26c54b4e0b0540316c6e
Wisdom <72468916+WisdomdeSagesse@users.noreply.github.com>
2022-11-24 00:36:36 +0000

    modified code to be more readable

--- main.py
+++ main.py
@@ -5,45 +5,45 @@ from replit import clear
 
 
 def rand_choice_description(choice):
-    details = []
-    details.append(choice['name'])
+    account_name = choice['name']
     if choice['description'][0] in ['a','e','i','o','u']:
-        details.append('an')
+        vowel = 'an'
     else:
-        details.append('a')
-    details.append(choice['description'])
-    details.append('from')
-    details.append(choice['country'])
-    description = ' '.join(details)
+        vowel = 'a'
+    account_desc = choice['description']
+    account_country = choice['country']
+    description = f"{account_name}, {vowel} {account_desc} from {account_country}"
     return description
 
 def rand_choice_value(choice):
     num_followers = choice['follower_count']
     return num_followers
 
-def compare_answer(user_pick,pick_a, pick_b):
+def compare_answer(pick_a, pick_b):
+    user_pick = input("Who has more followers? Type 'A' or 'B': ").lower()
+    while user_pick != 'a' and user_pick != 'b':
+        user_pick = input("You have entered a wrong key. Type 'A' or 'B': ").lower()
     if user_pick == 'a':
         return pick_a > pick_b
     elif user_pick == 'b':
         return pick_b > pick_a
+
 def play_game():  
   print(logo)
-  choice_a = rd.choice(data)    
   choice_b = rd.choice(data)
   score = 0
   end_game = False
   while not end_game:
+      choice_a = choice_b
+      choice_b = rd.choice(data)
+      while choice_a == choice_b:
+            choice_b = rd.choice(data)
       print(f"Compare A: {rand_choice_description(choice_a)}")
       print(vs)
       print(f"Against B: {rand_choice_description(choice_b)}")
-      user_pick = input("Who has more followers? Type 'A' or 'B': ").lower()
-      while user_pick != 'a' and user_pick != 'b':
-          user_pick = input("You have entered a wrong key. Type 'A' or 'B': ").lower()
-      if compare_answer(user_pick, rand_choice_value(choice_a), rand_choice_value(choice_b)) == True:
+      if compare_answer(rand_choice_value(choice_a), rand_choice_value(choice_b)) == True:
           score += 1
           print(f"You are correct. Current score: {score}")
-          choice_a = choice_b
-          choice_b = rd.choice(data)
           clear()
       else:
           print(f"You are wrong. Final score : {score}")
@@ -55,5 +55,8 @@ def play_game():
     clear()
     play_game()
   else:
+    clear()
     return
+
+
 play_game()   
